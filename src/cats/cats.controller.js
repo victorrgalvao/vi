8651de43b9806374697ecb91cef09ba29a376d46
roberto_farias <jaciofferton@gmail.com>
2023-08-08 15:35:20 -0300

    att 1

---
+++ src/cats/cats.controller.js
@@ -0,0 +1,43 @@
+import { Bind, Controller, Get, Param, Res, HttpStatus } from '@nestjs/common';
+
+const gatos = [
+    {
+        id: 1,
+        nome: "raposo",
+        corOlhos: "azul",
+        raca: "persa"
+    },
+    {
+        id: 2,
+        nome: "dora",
+        corOlhos: "azul",
+        raca: "angora"
+    },
+    {
+        id: 3,
+        nome: "iceberg",
+        corOlhos: "verde",
+        raca: "simames"
+    }
+]
+
+@Controller('cats')
+export class CatsController {
+    @Get()
+    findAll() {
+        return gatos;
+    }
+
+    @Get(':id')
+    @Bind(Param(), Res())
+    findOne(params, res) {
+        const CatData = gatos.find(gato => gato.id == Number(params.id));
+       
+        if (CatData) {
+            res.status(HttpStatus.OK).json(CatData);
+        } else {
+            res.status(HttpStatus.NOT_FOUND).send();
+
+        }
+    }
+}
